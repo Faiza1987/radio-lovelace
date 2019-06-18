232eaeea0d028ae409d1ce5fc87dad3ba31e7a70
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-06-18 09:00:08 -0700

    radio lovelace

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -4,53 +4,81 @@ import './styles/Playlist.css';
 
 import Track from './Track';
 
-const calculatePlayTime = (tracks) => {
-  let minutes = 0;
-  let seconds = 0;
-  tracks.forEach((track) => {
-    const times = track.playtime.split(':');
-    minutes += parseInt(times[0]);
-    seconds += parseInt(times[1]);
-  });
 
-  minutes += Math.floor(seconds / 60);
-  const hours = Math.floor(minutes / 60);
+class Playlist extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      // initial order of tracks
+      tracks: this.props.tracks,
+    }
+  }
+  // tracks will be re-ordered when a track is moved to the top
+  reOrderTracks = (track_index) => {
+    let tracks = this.state.tracks;
+    // removes track that is at track_index and moves it and saves it in the track variable
+    const track = tracks.splice(track_index, 1)[0];
 
-  seconds %= 60;
-  minutes %= 60;
+    // adds track to the beginning of the array (playlist)
+    tracks.unshift(track);
 
-  seconds = ("" + seconds).padStart(2, "0");
-  minutes = ("" + minutes).padStart(2, "0");
+    // sets state to reflect the new order of the songs
+    this.setState({
+      tracks: tracks
+    });
+  }
 
-  return `${hours}:${minutes}:${seconds}`;
-}
-
-const Playlist = (props) => {
-  const tracks = props.tracks;
-  const trackCount = tracks.length;
-  const playtime = calculatePlayTime(tracks);
-  const trackElements = tracks.map((track, i) => {
-    // We use "spread syntax" here to pass in all the properties of 
-    // the variable 'track' as props. Go look it up!
+  calculatePlayTime = (tracks) => {
+    let minutes = 0;
+    let seconds = 0;
+    tracks.forEach((track) => {
+      const times = track.playtime.split(':');
+      minutes += parseInt(times[0]);
+      seconds += parseInt(times[1]);
+    });
+  
+    minutes += Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+  
+    seconds %= 60;
+    minutes %= 60;
+  
+    seconds = ("" + seconds).padStart(2, "0");
+    minutes = ("" + minutes).padStart(2, "0");
+  
+    return `${hours}:${minutes}:${seconds}`;
+  }
+  render() {
+    const tracks = this.state.tracks;
+    const trackCount = tracks.length;
+    const playtime = this.calculatePlayTime(tracks);
+    const trackElements = tracks.map((track, i) => {
+      // We use "spread syntax" here to pass in all the properties of 
+      // the variable 'track' as props. Go look it up!
+      return (
+        <Track
+          //key={track.id}
+          key={i}
+          index={i}
+          moveToTopCallback={this.reOrderTracks}
+          {...track}
+  
+        />
+      );
+    });
     return (
-      <Track
-        key={track.id}
-        {...track}
-      />
+      <div className="playlist">
+        <h2>{this.props.side} Playlist</h2>
+        <p>
+          {trackCount} tracks - {playtime}
+        </p>
+        <ul className="playlist--track-list">
+          {trackElements}
+        </ul>
+      </div>
     );
-  });
+  }
 
-  return (
-    <div className="playlist">
-      <h2>{props.side} Playlist</h2>
-      <p>
-        {trackCount} tracks - {playtime}
-      </p>
-      <ul className="playlist--track-list">
-        {trackElements}
-      </ul>
-    </div>
-  );
 }
 
 Playlist.propTypes = {
--- src/components/Track.js
+++ src/components/Track.js
@@ -5,30 +5,59 @@ import "./styles/Track.css";
 
 // Here we use destructuring to extract the props into separate variables
 // See https://wesbos.com/destructuring-objects/
-const Track = ({title, artist, playtime, albumart, favorite}) => {
-  return (
-    <li className="track">
-      <img className="track--albumart" alt={`album art for ${title}`} src={albumart} />
-      <h3 className="track--title">{title}</h3>
-      <input
-        type="checkbox"
-        className="track--favorite"
-        checked={!favorite}
-      />
-      <p className="track--artist">{artist}</p>
-      <p className="track--playtime">{playtime}</p>
-      <button
-        className="track--control track--to-top"
-        >
-        <span role="img" aria-label="send to top">🔝</span>
-      </button>
-      <button
-        className="track--control track--switch"
-        >
-        <span role="img" aria-label="switch lists">↔</span>
-      </button>
-    </li>
-  );
+class Track extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      // title: props.title,
+      // artist: props.artist,
+      // playtime: props.playtime,
+      // albumart: props.albumart,
+      favorite: false,
+      index: props.index,
+    };
+  }
+
+  onFavoriteClick = () => {
+    this.setState({
+      favorite: true,
+    })
+  }
+
+  moveToTop = () => {
+    this.props.moveToTopCallback(this.state.index);
+  }
+
+  render(){
+    const {title, artist, playtime, albumart } = this.props;
+
+    return (
+      <li className="track">
+        <img className="track--albumart" alt={`album art for ${title}`} src={albumart} />
+        <h3 className="track--title">{title}</h3>
+        <input
+          type="checkbox"
+          className="track--favorite"
+          checked={!this.props.favorite}
+          onChange={this.onFavoriteClick}
+        />
+        <p className="track--artist">{artist}</p>
+        <p className="track--playtime">{playtime}</p>
+        <button
+          className="track--control track--to-top"
+          onClick={this.moveToTop}
+          >
+          <span role="img" aria-label="send to top">🔝</span>
+        </button>
+        <button
+          className="track--control track--switch"
+          >
+          <span role="img" aria-label="switch lists">↔</span>
+        </button>
+      </li>
+    );
+  }
 };
 
 Track.propTypes = {
